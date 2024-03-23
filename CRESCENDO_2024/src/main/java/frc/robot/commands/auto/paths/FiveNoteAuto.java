b54fa9cdf5b52315b59abcf58ea22d90e5a1eee4
BreakerBots <5104software@gmail.com>
2024-03-22 18:01:50 -0700

    PATHS
    Co-Authored-By: Aiden-Copiaco <156555503+Aiden-Copiaco@users.noreply.github.com>
    Co-Authored-By: Rev <53663385+reverset@users.noreply.github.com>

--- CRESCENDO_2024/src/main/java/frc/robot/commands/auto/paths/FiveNoteAuto.java
+++ CRESCENDO_2024/src/main/java/frc/robot/commands/auto/paths/FiveNoteAuto.java
@@ -4,23 +4,24 @@
 
 package frc.robot.commands.auto.paths;
 
+import java.util.Optional;
+
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.AutoConstants;
 import frc.robot.Vision;
 import frc.robot.commands.StationaryShootFromAnywhere;
 import frc.robot.commands.auto.actions.AutoAngleSnap;
-import frc.robot.commands.auto.actions.PersueAndIntakeNote;
 import frc.robot.commands.auto.actions.PersueAndIntakeNoteForShooter;
-import frc.robot.commands.handoffs.HandoffFromIntakeToShooter;
-import frc.robot.commands.shooter.SpoolShooterForSpeakerShot;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Intake.IntakeState;
-
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
@@ -28,41 +29,39 @@ import frc.robot.subsystems.Intake.IntakeState;
 public class FiveNoteAuto extends SequentialCommandGroup {
   /** Creates a new FiveNoteAuto. */
   public FiveNoteAuto(Shooter shooter, Drive drivetrain, Intake intake, Vision vision) {
+    var A2ToA1 = PathPlannerPath.fromPathFile("A2ToA1");
+    var A1ToA3 = PathPlannerPath.fromPathFile("A1ToA3");
+    var A3ToB4 = PathPlannerPath.fromPathFile("A3ToB4");
+    var B4ToSpeakerHigh = PathPlannerPath.fromPathFile("B4ToSpeakerHigh");
+    var SpeakerHighToB3 = PathPlannerPath.fromPathFile("SpeakerHighToB3UnderChain");
+    var B3ToSpeakerHigh = PathPlannerPath.fromPathFile("B3ToSpeakerHighUnderChain");
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-
-    var centerToBottomNote = PathPlannerPath.fromPathFile("Center_to_bottom_note");
-    var bottomNoteBackup = PathPlannerPath.fromPathFile("CenterNotebackup");
-    var a3_b5 = PathPlannerPath.fromPathFile("A3-B5");
-    var b5_speaker = PathPlannerPath.fromPathFile("B5-AgainstSpeaker");
-
-
     addCommands(
       new StationaryShootFromAnywhere(shooter, drivetrain),
-      intake.setStateCommand(IntakeState.EXTENDED_NEUTRAL, false),
-      AutoBuilder.followPath(centerToBottomNote),
-      new PersueAndIntakeNote(vision, shooter, intake, drivetrain),
-      AutoBuilder.followPath(bottomNoteBackup)
-        .alongWith(
-          new HandoffFromIntakeToShooter(shooter, intake, false)
-          .andThen(
-            new SpoolShooterForSpeakerShot(shooter, false)
-          )
-        ),
-      new StationaryShootFromAnywhere(shooter, drivetrain),
-
-      new AutoAngleSnap(Rotation2d.fromDegrees(50), drivetrain), 
+      new ConditionalCommand(
+        new AutoAngleSnap(Rotation2d.fromDegrees(0.0), drivetrain),
+        new AutoAngleSnap(Rotation2d.fromDegrees(180.0), drivetrain), () -> {
+          Optional<Alliance> allyOpt = DriverStation.getAlliance();
+          return allyOpt.isPresent() && allyOpt.get() == Alliance.Blue;
+        }),
       new PersueAndIntakeNoteForShooter(vision, shooter, intake, drivetrain),
       new StationaryShootFromAnywhere(shooter, drivetrain),
-
-      new AutoAngleSnap(Rotation2d.fromDegrees(90), drivetrain),
+      AutoBuilder.pathfindThenFollowPath(A2ToA1, AutoConstants.PATHFIND_TO_AUTOPATH_START_CONSTRAINTS),
       new PersueAndIntakeNoteForShooter(vision, shooter, intake, drivetrain),
       new StationaryShootFromAnywhere(shooter, drivetrain),
-
-      AutoBuilder.followPath(a3_b5),
+      AutoBuilder.pathfindThenFollowPath(A1ToA3, AutoConstants.PATHFIND_TO_AUTOPATH_START_CONSTRAINTS),
       new PersueAndIntakeNoteForShooter(vision, shooter, intake, drivetrain),
-      new SpoolShooterForSpeakerShot(shooter, false),
-      AutoBuilder.followPath(b5_speaker),
+      new StationaryShootFromAnywhere(shooter, drivetrain),
+      AutoBuilder.pathfindThenFollowPath(A3ToB4, AutoConstants.PATHFIND_TO_AUTOPATH_START_CONSTRAINTS),
+      new PersueAndIntakeNoteForShooter(vision, shooter, intake, drivetrain),
+      AutoBuilder.pathfindThenFollowPath(B4ToSpeakerHigh, AutoConstants.PATHFIND_TO_AUTOPATH_START_CONSTRAINTS),
+      new StationaryShootFromAnywhere(shooter, drivetrain),
+
+      // CUT THESE LINES OUT IF AUTOPATH TAKES TOO LONG
+      AutoBuilder.pathfindThenFollowPath(SpeakerHighToB3, AutoConstants.PATHFIND_TO_AUTOPATH_START_CONSTRAINTS),
+      new PersueAndIntakeNoteForShooter(vision, shooter, intake, drivetrain),
+      AutoBuilder.pathfindThenFollowPath(B3ToSpeakerHigh, AutoConstants.PATHFIND_TO_AUTOPATH_START_CONSTRAINTS),
       new StationaryShootFromAnywhere(shooter, drivetrain)
     );
   }
