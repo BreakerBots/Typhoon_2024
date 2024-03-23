1050b126602f2ce2b0714beeae0543369fd170f5
BreakerBots <5104software@gmail.com>
2024-03-22 19:22:05 -0700

    LED
    Co-Authored-By: Rev <53663385+reverset@users.noreply.github.com>

--- CRESCENDO_2024/src/main/java/frc/robot/subsystems/LED.java
+++ CRESCENDO_2024/src/main/java/frc/robot/subsystems/LED.java
@@ -4,34 +4,68 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.BreakerLib.devices.cosmetic.led.BreakerRevBlinkin;
+import frc.robot.BreakerLib.devices.cosmetic.led.BreakerRevBlinkin.FixedPalettePattern;
+import frc.robot.BreakerLib.devices.cosmetic.led.BreakerRevBlinkin.SolidColor;
 
 public class LED extends SubsystemBase {
   public enum LEDState {
-    INTAKING_FOR_SHOOTER,
-    INTAKING_FOR_AMP,
-    EXTAKING,
+    INTAKING_FOR_SHOOTER(FixedPalettePattern.BREATH_GRAY),
+    INTAKING_FOR_AMP(FixedPalettePattern.BREATH_GRAY),
+    EXTAKING(FixedPalettePattern.HEARTBEAT_BLUE),
     
-    SHOOTING_TO_SPEAKER,
-    SCORING_IN_AMP,
-    PERSUING_NOTE,
+    SHOOTING_TO_SPEAKER(FixedPalettePattern.FIRE_LARGE),
+    SCORING_IN_AMP(FixedPalettePattern.STROBE_GOLD),
+    PERSUING_NOTE(FixedPalettePattern.RAINBOW_GLITTER),
     
-    ENABLED_WITH_NOTE,
-    ENABLED,
-    DISABLED,
+    ENABLED_WITH_NOTE(FixedPalettePattern.FIRE_MEDIUM),
+    ENABLED(FixedPalettePattern.BREATH_BLUE),
     
-    SPOOLING_FLYWHEEL,
+    SPOOLING_FLYWHEEL(FixedPalettePattern.CONFETTI),
 
-    ERROR,
+    ERROR(FixedPalettePattern.HEARBEAT_RED);
+
+    private FixedPalettePattern pattern;
+
+
+    private LEDState(FixedPalettePattern pattern) {
+      this.pattern = pattern;
+    }
+
+    public FixedPalettePattern getFixedPalettePattern() {
+      return pattern;
+    }
 
   }
   
   private final BreakerRevBlinkin blinkin;
+  private LEDState currentState;
 
   /** Creates a new LED. */
   public LED() {
     blinkin = new BreakerRevBlinkin(0);
+    currentState = LEDState.ENABLED;
+  }
+
+  public Command setStateCommand(LEDState state) {
+    return runOnce(() -> setState(state));
+  }
+
+  public void setState(LEDState state) {
+    currentState = state;
+    updateBlinkin();
+  }
+
+  private void updateBlinkin() {
+    blinkin.setFixedPalettePattern(currentState.getFixedPalettePattern());
+  }
+
+  public LEDState getCurrentState() {
+    return currentState;
   }
 
   @Override
